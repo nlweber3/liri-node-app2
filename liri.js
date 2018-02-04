ebd91e8ad159d2bcd56c09cd33d946ed659361e2
Nick <nlweber3@gmail.com>
2018-02-03 20:40:36 -0800

    still having issues

--- liri.js
+++ liri.js
@@ -3,25 +3,29 @@ require('dotenv').config();
 var request = require('request');
 var fs = require('fs');
 var Twitter = require('twitter');
-var Spotify = require('Spotify');
+// var Spotify = require('Spotify');
 var userInput = process.argv[2];
+// tweets();
 
-function tweets() {
-	var params = {screen_name: process.argv[3]};
+	var params = {screen_name: 'artisticbinary'};
 	var client = new Twitter({
 		consumer_key: process.env.TWITTER_CONSUMER_KEY,
 		consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
 		access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
 		access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 	});
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	client.get('statuses/tweets', params, function(error, tweets, response) {
 		if (!error) {
-			for (var i=0; i<20; i++) {
-				console.log(tweets[i].text);
-			}
+			// for (var i=0; i<20; i++) {
+				console.log("yay");
+			// }
+		}
+		else {
+		tweetsNull();
+		console.log(error);
 		}
 	});
-}
+
 function tweetsNull() {
 	var params = {screen_name: "N/A"};
 	var client = new Twitter({
