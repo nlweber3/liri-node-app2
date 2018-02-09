8b4c342085feb15304f6d330e76bb03629cba338
Nick <nlweber3@gmail.com>
2018-02-08 18:04:46 -0800

    spotify returing something

--- liri.js
+++ liri.js
@@ -5,13 +5,20 @@ var fs = require('fs');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 
+
+//function for choosing api call
 function checkforinput() {
 	if (process.argv[2] = tweets) {
 		tweets();
 	}
+	else if (process.argv[2] = spotify) {
+		spotify();
+	}
 
 };
 
+
+//twitter api
 function tweets() {
 	var params = {screen_name: process.argv[3]};
 	var client = new Twitter({
@@ -28,10 +35,31 @@ function tweets() {
 			}
 		}
 		else {
-		console.log(error);
+			console.log(error);
 		}
 	});
 
 }
-// checkForOption();
+
+
+var spotify = new Spotify({
+	id: process.env.SPOTIFY_ID,
+	secret: process.env.SPOTIFY_SECRET
+});
+
+
+//spotify api
+function spotify() {
+spotify.search({ type: 'track', query: process.argv[3], limit:1 }, function(err, data) {
+	if (error) {
+		return console.log('Error occurred: ' + err);
+	}
+
+	console.log(data.tracks.items[0]); 
+});
+}
+
+
+
+
 checkforinput();
